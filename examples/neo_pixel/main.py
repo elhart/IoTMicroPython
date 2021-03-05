9ac7ac49ffd15c1b8183e604235f917550f5461a
elhart <ivan.elhart@gmail.com>
2021-03-05 22:15:30 +0100

    neo_pixel update

--- examples/neo_pixel/main.py
+++ examples/neo_pixel/main.py
@@ -6,8 +6,13 @@ n_led = 8
 
 np = NeoPixel(Pin(pin), n_led)
 
-np[0] = (64, 0, 0)     # set to red, quarter brightness
-np[1] = (0, 64, 0)     # set to green, quarter brightness
-np[2] = (0, 0, 64)     # set to blue, quarter brightness
+np[0] = (64, 0, 0)      # set to red
+np[1] = (64, 32, 0)     # set to orange
+np[2] = (64, 64, 0)     # set to yellow
+np[3] = (0, 64, 0)      # set to green
+np[4] = (0, 0, 64)      # set to blue
+np[5] = (38, 0, 64)
+np[6] = (38, 0, 52)
+np[7] = (0, 0, 24)
 
 np.write()
