8b65fd77a8befb504d951e5967869ccfe5e6cfae
elhart <ivan.elhart@gmail.com>
2021-03-07 21:37:31 +0100

    esp8266 neo_pixel

--- examples/neo_pixel/main.py
+++ examples/neo_pixel/main.py
@@ -1,13 +1,23 @@
+"""
+Example how to use NeoPixel library to drive an array of LEDs
+using esp8266 and esp32 development boards
+"""
+
 from time import sleep
 from machine import Pin
 from neopixel import NeoPixel
 
+# on esp8266, pin 4 is labeled D2
+# on esp32, pin 4 is D4
 pin = 4
 n_led = 8
 
 np = NeoPixel(Pin(pin), n_led)
 
 def rainbow():
+    """
+    Setting all 8 led to different colors
+    """
     np[0] = (64, 0, 0)      # set to red
     np[1] = (64, 32, 0)     # set to orange
     np[2] = (64, 64, 0)     # set to yellow
@@ -23,6 +33,9 @@ def clear():
         np[pixel] = (0, 0, 0)
 
 def spin():
+    """
+    A single pixel (lef) moving along the array
+    """
     while True:
         for pixel in range(0, n_led):
             clear()
